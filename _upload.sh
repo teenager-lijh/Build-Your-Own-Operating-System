4053e818ad4715fb61327a0000f9b0a3778b4676
lijh <teenager_lijh@sina.com>
2022-01-26 23:14:34 -0800

    内核 put_char 函数运行成功

---
+++ _upload.sh
@@ -0,0 +1,16 @@
+gcc -m32 -c -o /home/lijh/Build-Your-Own-Operating-System/kernel/main.o \
+/home/lijh/Build-Your-Own-Operating-System/kernel/hello.c
+
+echo "gcc success \n"
+
+ld /home/lijh/Build-Your-Own-Operating-System/kernel/main.o -Ttext 0xc0001500 -e main -o /home/lijh/Build-Your-Own-Operating-System/kernel/kernel.bin -m elf_i386
+
+echo "ld success"
+
+dd if=/home/lijh/Build-Your-Own-Operating-System/kernel/kernel.bin of=/home/lijh/Build-Your-Own-Operating-System/hd60M.img bs=512 count=200 seek=9 conv=notrunc
+
+
+echo "dd success"
+
+
+
