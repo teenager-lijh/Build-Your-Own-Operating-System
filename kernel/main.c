df5bcbb81f15979896db1a43392818f1bdcbf1f8
lijh <teenager_lijh@sina.com>
2022-02-17 00:16:09 -0800

    c8/a 使用makefile构建项目

--- kernel/main.c
+++ kernel/main.c
@@ -1,9 +1,11 @@
 #include "print.h"
 #include "init.h"
-void main(void) {
+#include "debug.h"
+int main(void) {
    put_str("I am kernel\n");
    init_all();
-   asm volatile("sti");	     // 为演示中断处理,在此临时开中断
+   ASSERT(1==2);
    while(1);
+   return 0;
 }
 
