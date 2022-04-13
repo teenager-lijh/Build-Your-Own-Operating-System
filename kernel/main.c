85c2d55999cf2e9aef5acffea83a3ce0a7fbd615
lijh <teenager_lijh@sina.com>
2022-04-13 22:01:47 +0800

    c8/e over 完善 memory.c/h

--- kernel/main.c
+++ kernel/main.c
@@ -4,7 +4,11 @@
 int main(void) {
    put_str("I am kernel\n");
    init_all();
-    mem_init(); 
+
+   void* addr = get_kernel_pages(3);
+   put_str("\n get_kernel_page start vaddr is ");
+   put_int((uint32_t)addr);
+   put_str("\n");
 
    while(1);
    return 0;
