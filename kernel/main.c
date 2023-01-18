ed10a1faeece7094c315a3a540c923db92cc3a43
lijh <teenager_lijh@sina.com>
2023-01-19 00:17:19 +0800

    c10/a over 多线程调度应该区分哪些内容是原子操作

--- kernel/main.c
+++ kernel/main.c
@@ -13,8 +13,11 @@ int main(void) {
    thread_start("k_thread_b", 8, k_thread_b, "argB ");
 
    intr_enable();	// 打开中断,使时钟中断起作用
+
    while(1) {
+      intr_disable();	 // 关中断
       put_str("Main ");
+      intr_enable();	 // 开中断
    };
    return 0;
 }
@@ -24,7 +27,9 @@ void k_thread_a(void* arg) {
 /* 用void*来通用表示参数,被调用的函数知道自己需要什么类型的参数,自己转换再用 */
    char* para = arg;
    while(1) {
+      intr_disable();	 // 关中断
       put_str(para);
+      intr_enable();	 // 开中断
    }
 }
 
@@ -33,6 +38,9 @@ void k_thread_b(void* arg) {
 /* 用void*来通用表示参数,被调用的函数知道自己需要什么类型的参数,自己转换再用 */
    char* para = arg;
    while(1) {
+      intr_disable();	 // 关中断
       put_str(para);
+      intr_enable();	 // 开中断
    }
 }
+
